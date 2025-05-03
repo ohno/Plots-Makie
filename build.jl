fb2584cbc31034eb1c5b0b648eb668453edce88b
Shuhei Ohno <ohno.github@gmail.com>
2025-05-03 15:46:55 +0900

    Add new examples

--- build.jl
+++ build.jl
@@ -3,15 +3,23 @@ println("begin")
 io = open("./README.md", "w")
 println(io, "# Plots-Makie")
 
-for dir in ["templete", "hello", "scatter", "heatmap"]
+for dir in ["templete", "hello", "lines", "scatter", "histogram", "surface", "heatmap", "subplot", "grid", "text", "artistic", "animation"]
   cd(dir)
+  extension =
+    if dir == "animation"
+      "gif"
+    elseif dir == "surface"
+      "png"
+    else
+      "svg"
+    end
   println(pwd())
   println(io, "")
   println(io, "## [$(uppercasefirst(dir))](./$(dir))")
   println(io, "\n### [Plots.jl](./$(dir)/plots.jl)")
-  println(io, "![](./$(dir)/plots.svg)")
+  println(io, "![](./$(dir)/plots.$(extension))")
   println(io, "\n### [Makie.jl](./$(dir)/makie.jl)")
-  println(io, "![](./$(dir)/makie.svg)")
+  println(io, "![](./$(dir)/makie.$(extension))")
   include("./$(dir)/$(ARGS[1]).jl")
   cd("..")
 end
