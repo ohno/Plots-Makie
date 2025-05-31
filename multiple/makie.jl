f3e6a204802be85cd32e68ee0daa09ee9748c694
Shuhei Ohno <ohno.github@gmail.com>
2025-06-01 06:19:27 +0900

    Update multiple

--- multiple/makie.jl
+++ multiple/makie.jl
@@ -2,8 +2,8 @@ using CairoMakie
 
 fig = Figure()
 axis = Axis(fig[1,1])
-lines!(axis, 0:0.1:1, sin, label="line")
-scatter!(axis, 0:0.1:1, sin, label="scatter")
+lines!(axis, 0..5, x -> x^2, label="line")
+scatter!(axis, [1,2,3,4], [1,5,8,17], label="scatter")
 axislegend(axis, position=:rb, framevisible=false)
 
 save("./makie.svg", fig)
--- multiple/plots.jl
+++ multiple/plots.jl
@@ -1,7 +1,7 @@
 using Plots
 
 plt = plot(legend = :bottomright)
-plot!(plt, 0:0.1:1.0, sin, label = "line")
-scatter!(plt, 0:0.1:1.0, sin, label = "scatter")
+plot!(plt, 0:0.1:5, x -> x^2, label = "line")
+scatter!(plt, [1,2,3,4], [1,5,8,17], label = "scatter")
 
 savefig("./plots.svg")
